0bc8e4af6f0ebe1334707ac136210a967aed79c3
PraphulSamavedam <praphulsamavedam@gmail.com>
2024-06-20 12:42:20 -0400

    Adds the brute force solution for June 20th 2024.

--- Leetcode/Daily Questions/June 20 2024/solution.py
+++ Leetcode/Daily Questions/June 20 2024/solution.py
@@ -3,21 +3,47 @@ from typing import List
 
 class Solution:
 
+    def brute_approach(self, positions: List[int], m: int) -> int:
+        """
+        This method returns the maximum possible minimum distance between
+        any two balls placed in the positions after placing m balls in the
+        positions provided.
+
+        Time complexity: O(n * max(positions) ) where n = len(positions)
+        Space complexity: O(1)
+        """
+        positions.sort(reverse=False)
+        best_max_dist = positions[-1] - positions[0]
+
+        ans = -1
+        for dist in range(1, best_max_dist + 1):
+            balls = 1
+            last_posn = positions[0]
+            for posn in positions:
+                if posn - last_posn >= dist:
+                    last_posn = posn
+                    balls += 1
+            if balls >= m:
+                ans = dist
+            else:  # balls < m
+                break
+        return ans
+
     def optimal_approach(self, positions: List[int], m: int) -> int:
         """
         This method returns the maximum possible minimum distance between
         any two balls placed in the positions after placing m balls in the
         positions provided.
 
-        Time complexity: O(n * (max(positions) - min(positions) )
+        Time complexity: O(n * log (max(positions) - min(positions)) )
         Space complexity: O(1)
         """
         positions.sort(reverse=False)
-        lt, rt = 1, positions[-1] - positions[0] # Min Distance, Max Distance
+        lt, rt = 1, positions[-1] - positions[0]  # Min Distance, Max Distance
 
         ans = -1
         while lt <= rt:
-            mid = lt + (rt - lt)//2
+            mid = lt + (rt - lt) // 2
             last_posn = positions[0]
             balls = 1
             for posn in positions:
@@ -39,6 +65,17 @@ class Solution:
         results = [299, 99, 3, 1]
         soln_results = []
         test_fail = []
+
+        # Brute Force approach
+        print(f"Brute Force Approach")
+        for indx, _ in enumerate(input_inps):
+            soln_result = self.brute_approach(positions=input_inps[indx],
+                                              m=m_inps[indx])
+            test_fail.append(soln_result == results[indx])
+            soln_results.append(soln_result)
+        self.print_results(results, soln_results, test_fail)
+        soln_results.clear()
+        test_fail.clear()
 
         # Optimal approach
         print(f"Optimal Approach")
